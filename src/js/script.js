3bdac729bc2f4b70ee0f2042188c5354ad8e2369
GeovanniMarques <geovannimarques1@gmail.com>
2026-02-04 18:55:39 -0300

    Instanciamento de menu responsivo

--- src/js/script.js
+++ src/js/script.js
@@ -49,6 +49,52 @@ window.addEventListener("scroll", () => {
 //     });
 // });
 
+//  ======= BARRA DE NAVEGAÇÃO MOBILE =======
+class MobileNavBar {
+    constructor(mobileMenu, navList, navLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    animateLinks() {
+        this.navLinks.forEach((link, index) => {
+            link.style.animation
+                ? (link.style.animation = "")
+                : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+                      index / 7 + 0.3 // Personalização de velocidade de animação
+                  }s`);
+        });
+    }
+
+    handleClick() {
+        this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
+        this.animateLinks();
+    }
+
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+
+    init() {
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
+
+const mobileNavBar = new MobileNavBar(
+    ".mobile-menu",
+    ".links",
+    ".links li"
+);
+mobileNavBar.init();
+
 //  ======= VISIBILIDADE DO BOTÃO PARA VOLTAR AO TOPO =======
 const bntVoltarTopo = document.getElementById("botao-voltar-topo");
 
